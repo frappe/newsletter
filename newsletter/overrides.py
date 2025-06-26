2d129d19cf26b7bcfb440c1f236fe679bb632c9c
sokumon <sohamkulkarns9@gmail.com>
2025-06-27 01:38:36 +0530

    chore: run linter

--- newsletter/overrides.py
+++ newsletter/overrides.py
@@ -1,4 +1,5 @@
 import frappe
 
+
 def website_context(context):
-    context.is_erpnext_installed = "erpnext" in frappe.get_installed_apps()
+	context.is_erpnext_installed = "erpnext" in frappe.get_installed_apps()
